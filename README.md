9dbfa86a2ae2c20328e56ecf85b467560a36b5cd
DtxdF <josefn2310@gmail.com>
2019-12-25 10:45:10 -0400

    Lista de cambios: * Procesos en paralelo para una mayor eficacia * Removido, usar un zip cómo diccionario. Una decisión subjetiva, porque prefiero que el usuario se descargue su propio diccionario, en vez de tener uno pre-determinado. * Agregado, Multiples algoritmos * Mayor rendimiento en cuanto al código
    ~ DtxdF

--- README.md
+++ README.md
@@ -1,7 +1,56 @@
 # Erica
-Este es un pequeño programa para descifrar contraseñas mediante hashes. Por ahora, solo esta disponible en MD5.
+Erica es una herramienta para realizar ataques por diccionario contra diferentes familias de algoritmos hash
 
-# Como usar
-```python erica.py -p HASH -f file```
+# Algoritmos Válidos
+* MD5
+* SHA (1, 224, 256, 384, 512)
+* SHA-3 (224, 256, 384, 512)
+* shake (128, 256)
+* blake2b & blake2s
 
-Ejemplo: ```python erica.py -p ccee5504c9d889922b101124e9e43b71 -f passwords.txt```
+# Requerimientos
+* Python 3.x
+* Colorama (Sólo para Windows)
+
+# Modo de ejecución
+```bash
+python3 ./erica.py -h
+ 
+  ▓█████  ██▀███   ██▓ ▄████▄   ▄▄▄      
+  ▓█   ▀ ▓██ ▒ ██▒▓██▒▒██▀ ▀█  ▒████▄    
+  ▒███   ▓██ ░▄█ ▒▒██▒▒▓█    ▄ ▒██  ▀█▄  
+  ▒▓█  ▄ ▒██▀▀█▄  ░██░▒▓▓▄ ▄██▒░██▄▄▄▄██ 
+  ░▒████▒░██▓ ▒██▒░██░▒ ▓███▀ ░ ▓█   ▓██▒
+  ░░ ▒░ ░░ ▒▓ ░▒▓░░▓  ░ ░▒ ▒  ░ ▒▒   ▓▒█░
+   ░ ░  ░  ░▒ ░ ▒░ ▒ ░  ░  ▒     ▒   ▒▒ ░
+     ░     ░░   ░  ▒ ░░          ░   ▒   
+     ░  ░   ░      ░  ░ ░            ░  ░
+
+  Created by: Kirari
+ 
+usage: erica.py [-h] [-l LONG] [-p PROCESS] -H HASH -a ALGORITHM -w WORDLIST
+
+Erica es una herramienta que te permitra crackear muchas familias de
+algoritmos con sus respectivas versiones. Entre los cuales se encuentran: MD5,
+SHA (1, 224, 256, 384, 512), SHA-3 (224, 256, 384, 512), shake (128, 256),
+blake2b & blake2s.
+
+optional arguments:
+  Opcionales
+
+  -h, --help            Mostrar la ayuda y salir
+  -l LONG, --long LONG  Para el caso de la familia "shake" se debe colocar
+                        explicitamente la longitud. Pre-determinado: 2
+  -p PROCESS, --process PROCESS
+                        Número de procesos en paralelo. ADVERTENCIA: Por favor
+                        hagalo bajo su propio cuidado. Pre-determinado: 2
+
+Argumentos principales:
+  -H HASH, --hash HASH  El hash para crackear
+  -a ALGORITHM, --algorithm ALGORITHM
+                        El algoritmo a utilizar
+  -w WORDLIST, --wordlist WORDLIST
+                        La ruta del diccionario a utilizar
+```
+
+**Ejemplo:** python3 ./erica.py -H \<hash\> -a \<Algoritmo\> -w \<Ruta del diccionario\>
--- erica.py
+++ erica.py
@@ -1,22 +1,82 @@
-# -*- coding: utf-8 -*- 
+#!/usr/bin/env python3
 
-# Creado por Kirari Senpai
-
-
-import sys
-import time
-import hashlib
+import signal
 import argparse
-from os import system as s
+import sys
+import multiprocessing
 
+from inspect import isgenerator, isbuiltin
+from os import name as whoami
+from os.path import isfile
+from _io import TextIOWrapper
+from hashlib import algorithms_available
+from hashlib import (
+                        md5,
+                        # Sha family
+                        sha1,
+                        sha224,
+                        sha256,
+                        sha384,
+                        sha512,
+                        # Sha-3 family
+                        sha3_224,
+                        sha3_256,
+                        sha3_384,
+                        sha3_512,
+                        # shake family
+                        shake_128,
+                        shake_256,
+                        # Blake family
+                        blake2b,
+                        blake2s
 
-# password_md5 = (input("\033[1;39m[\033[1;31m-\033[1;39m] Ingrese hash de password:\033[0;39m ")).strip()
+                        )
 
+class HashNotFound(Exception):
+    """Llamado cuando el hash no existe"""
+
+class colors(object):
+    if (whoami != 'nt'):
+        GREEN = '\033[1;32m'
+        RED = '\033[1;31m'
+        BLUE = '\033[1;34m'
+        YELLOW = '\033[1;33m'
+        CYAN = '\033[1;36m'
+        WHITE = '\033[0;37m'
+        RESET = '\033[0m'
+
+    else:
+        import colorama
+        colorama.init()
+
+        GREEN = colorama.Fore.GREEN
+        RED = colorama.Fore.RED
+        BLUE = colorama.Fore.BLUE
+        YELLOW = colorama.Fore.YELLOW
+        CYAN = colorama.Fore.CYAN
+        WHITE = colorama.Fore.WHITE
+        RESET = colorama.Style.RESET_ALL
+
+def printI(string):
+    print('{}[{}*{}] - '.format(
+        colors.WHITE, colors.BLUE, colors.WHITE), string, flush=True)
+    print(colors.RESET, flush=True, end='')
+
+def printS(string):
+    print('{}[{}*{}] - '.format(
+        colors.WHITE, colors.GREEN, colors.WHITE), string, flush=True)
+    print(colors.RESET, flush=True, end='')
+
+def printE(string):
+    print('{}[{}-{}] - '.format(
+        colors.WHITE, colors.RED, colors.WHITE), string, flush=True)
+    print(colors.RESET, flush=True, end='')
+
+def protect(signum, frame):
+    sys.exit(0)
 
 def banner():
-
-	cartel = """\033[0;36m
- 
+    print(colors.CYAN, '''
   ▓█████  ██▀███   ██▓ ▄████▄   ▄▄▄      
   ▓█   ▀ ▓██ ▒ ██▒▓██▒▒██▀ ▀█  ▒████▄    
   ▒███   ▓██ ░▄█ ▒▒██▒▒▓█    ▄ ▒██  ▀█▄  
@@ -27,63 +87,94 @@ def banner():
      ░     ░░   ░  ▒ ░░          ░   ▒   
      ░  ░   ░      ░  ░ ░            ░  ░
 
-            \033[1;33mCreated by: Kirari
+''', colors.YELLOW, 'Created by: Kirari\n', colors.RESET)
 
+def crack(args):
+    (password_hash, wordfile, hash_func, longcode) = args
 
-	"""
+    with open(wordfile, 'rt') as wordlist:
+        if not (isinstance(wordlist, list)) \
+                and not (isgenerator(wordlist)) \
+                and not (isinstance(wordlist, TextIOWrapper)):
+            raise TypeError('El tipo de dato de la lista de hashes no es correcto')
 
-	print(cartel)
+        if not (hash_func in algorithms_available):
+            raise HashNotFound('El hash no existe')
 
+        else:
+            hash_ = globals()[hash_func]
+            if not (isbuiltin(hash_)) and not (isinstance(hash_, type)):
+                raise ValueError('{}, no es un dato correcto para ser una función hash'.format(hash_func))
 
-def searching_hash(password_hash,password_file):
+        for _ in wordlist:
+            _ = _.strip()
+            string_to_hash = hash_(_.encode())
+            string_to_hash = string_to_hash.hexdigest(longcode) \
+                    if (hash_func.startswith('shake_')) else string_to_hash.hexdigest()
 
-	time_start = time.time()
+            printI('Probando -> {}{} {}?= {}{}: {}{}'.format(
+                colors.YELLOW, string_to_hash, colors.BLUE, 
+                colors.YELLOW, password_hash, colors.GREEN, _))
 
-	word_crack = []
-	words = open(password_file, "rb")
+            if (string_to_hash == password_hash):
+                printS('Crackeado -> {} = {}'.format(
+                    _, string_to_hash))
+                return(True)
 
-	try:
-		for word in words.readlines():
-			hashed = hashlib.md5(word.strip())
-			word_hash = str(hashed.hexdigest())
+        printE('No hubo resultados satisfactorios...', flush=True)
+        return(False)
 
-			if(password_hash==word_hash):
-				#s('clear')
-				#banner()
-				print("\033[0;39m\n\n [\033[0;33m+\033[0;39m] Password encontrada: \033[1;36m" + str(word.decode()) + "\n\n")
-				word_crack.append(word)
-				break
-			else:
-				#s('clear')
-				#banner()
-				print("\033[1;33m Probando:  \033[0;32m" + str(word.decode()).strip() + " \033[0;32m " + word_hash)
-				#time.sleep(0.005)
-	except UnicodeDecodeError:
-		pass 
+def main():
+    banner()
 
-	if(word_crack==[]):
-		s('clear')
-		banner()
-		print("\033[0;39m\n\n [\033[0;31mx\033[0;39m] No se pudo encontrar la password\n\n")
+    DEFAULT_LONG = 2
+    DEFAULT_PROCESS = 2
 
-	words.close()
+    parser = argparse.ArgumentParser()
+    parser.description = 'Erica es una herramienta que te permitra crackear muchas familias de algoritmos con sus respectivas versiones. Entre los cuales se encuentran: MD5, SHA (1, 224, 256, 384, 512), SHA-3 (224, 256, 384, 512), shake (128, 256), blake2b & blake2s.'
+    parser._optionals.description = 'Opcionales'
+    parser._actions[0].help = 'Mostrar la ayuda y salir'
 
-	time_end = time.time()
+    parser.add_argument('-l', '--long', help='Para el caso de la familia "shake" se debe colocar explicitamente la longitud. Pre-determinado: {}'.format(DEFAULT_LONG), type=int, default=DEFAULT_LONG)
+    parser.add_argument('-p', '--process', help='Número de procesos en paralelo. ADVERTENCIA: Por favor hagalo bajo su propio cuidado. Pre-determinado: {}'.format(DEFAULT_PROCESS), type=int, default=DEFAULT_PROCESS)
 
-	final_time = (time_end - time_start)
+    primary = parser.add_argument_group('Argumentos principales')
+    primary.add_argument('-H', '--hash', help='El hash para crackear', required=True)
+    primary.add_argument('-a', '--algorithm', help='El algoritmo a utilizar', required=True)
+    primary.add_argument('-w', '--wordlist', help='La ruta del diccionario a utilizar', required=True)
 
-	print(" \033[0;37mTiempo transcurrido: {} segundos\n\n".format(final_time))
+    args = parser.parse_args()
 
+    if not (isfile(args.wordlist)):
+        printE('¡El diccionario no existe!')
+        sys.exit(1)
 
+    try:
+        pool = multiprocessing.Pool(args.process)
+        pool.map(crack, [(args.hash, args.wordlist,
+                args.algorithm, args.long)])
 
-banner()
-parser = argparse.ArgumentParser(description="Programa para crackear passwords mediante MD5")
-parser.add_argument('-p', '--hash', help="Password Hash MD5", required=True, type=str)
-#parser.add_argument('-m', '--mode', help="Elegir metodo de crackeo: wordlist, bruteforce.", required=True, type=str)
-parser.add_argument('-f', '--file', help="Elegir ruta de archivo de claves", required=True, type=str)
-argv = parser.parse_args()
+    except Exception as Except:
+        printE('Error: {}'.format(Except))
+        sys.exit(1)
 
-if(len(argv.hash)==32 and argv.file):
-	searching_hash(argv.hash,argv.file)
-else:
-	print("[\033[1;31m+\033[0;39m] Dato erroneo")
+if __name__ == '__main__':
+    signals = [
+            
+                signal.SIGINT,
+                signal.SIGTERM
+                ]
+
+    if (whoami != 'nt'):
+        signals.extend([
+            
+            signal.SIGQUIT,
+            signal.SIGUSR1,
+            signal.SIGUSR2
+            
+        ])
+
+    for _ in signals:
+        signal.signal(_, protect)
+
+    main()
